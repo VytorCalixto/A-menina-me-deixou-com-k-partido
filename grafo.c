a5f0ebcc3362033d27437a29c63b599792ae4984
Victor Perszel <vlp14@inf.ufpr.br>
2016-04-02 03:19:05 -0300

    Começo de leitura do grafo

--- grafo.c
+++ grafo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <graphviz/cgraph.h>
 #include "grafo.h"
 
 typedef struct grafo {
@@ -28,6 +29,25 @@ char *nome_vertice(vertice v){
   return v.nome;
 }
 
+grafo le_grafo(FILE *input){
+  //Pia, to com mto sono enquanto escrevo isso, n me julgue aoshfasfj
+  char buff[255];
+  grafo g;
+  fscanf(input, "%s", buff); // le a primeira palavra
+  if(buff == "digraph") // direcionado?
+    g.direcionado = 1;
+  else
+    g.direcionado = 0;
+  fscanf(input, "%s", buff); // le a segunda palavra
+  g.nome = buff; // que é o nome do grafo
+
+  // TODO: Ler vertices, arestas e pesos;
+}
+
 int destroi_grafo(void *g){
   free(g);
+  g = NULL;
+  return 1;
+  // N tem como retornar 0 caso ocorra erro
+  // Depois de um free ocorre undefined behaviour
 }
