0f24044296bd7a9a3bc96009ba29e728f7fade1e
Vytor Calixto <vytorcalixto@gmail.com>
2016-04-02 23:12:41 -0300

    n_vertices e n_arestas

--- grafo.c
+++ grafo.c
@@ -4,36 +4,58 @@
 #include "lista.h"
 #include <string.h>
 
-typedef struct grafo {
-  int direcionado;
-  int ponderado;
-  char* nome;
-  lista vertices;
-};
-
-char *nome_grafo(grafo g){
-  return g->nome;
-}
-
-int direcionado(grafo g){
-  return g->direcionado;
-}
-
-int ponderado(grafo g){
-  return g->ponderado;
-}
 
 typedef struct vertice{
-  char* nome;
-  unsigned int grau; // Para grafos não-direcionados
-  unsigned int grau_ent; // Entrada dos direcionados
-  unsigned int grau_sai; // Saída dos direcionados
+    char* nome;
+    unsigned int grau; // Para grafos não-direcionados
+    unsigned int grau_ent; // Entrada dos direcionados
+    unsigned int grau_sai; // Saída dos direcionados
 
-  lista arestas;
+    lista arestas;
 };
 
 char *nome_vertice(vertice v){
-  return v->nome;
+    return v->nome;
+}
+
+typedef struct grafo {
+    int direcionado;
+    int ponderado;
+    char* nome;
+    lista vertices;
+};
+
+char *nome_grafo(grafo g){
+    return g->nome;
+}
+
+int direcionado(grafo g){
+    return g->direcionado;
+}
+
+int ponderado(grafo g){
+    return g->ponderado;
+}
+
+unsigned int n_vertices(grafo g) {
+    unsigned int soma = 0;
+    for(no n=primeiro_no(g->vertices); n; n=proximo_no(n)) {
+        ++soma;
+    }
+
+    return soma;
+}
+
+unsigned int n_arestas(grafo g) {
+    unsigned int soma = 0;
+    for(no n=primeiro_no(g->vertices); n; n=proximo_no(n)) {
+        vertice v = (vertice) conteudo(n);
+        for(no p=primeiro_no(v->arestas); p; p=proximo_no(p)) {
+            ++soma;
+        }
+    }
+
+    return g->direcionado ? soma : (soma/2);
 }
 
 grafo le_grafo(FILE *input){
@@ -59,7 +81,7 @@ grafo le_grafo(FILE *input){
     }
 
     // Para cada vértice insere suas arestas
-    for(no n=primeiro_no(gf->vertices); n; n=n->proximo) {
+    for(no n=primeiro_no(gf->vertices); n; n=proximo_no(n)) {
         vertice v = (vertice) conteudo(n);
         Agnode_t *node = agnode(g, v->nome, FALSE);
         for(Agedge_t *e=agfstout(g, node); e; e=agnxtout(g, e)) {
@@ -75,34 +97,34 @@ grafo le_grafo(FILE *input){
 }
 
 int destroi_grafo(void *g){
-  free(g);
-  g = NULL;
-  return 1;
-  // N tem como retornar 0 caso ocorra erro
-  // Depois de um free ocorre undefined behaviour
-  //
-  // TODO: Passar por todos os vertices desalocando
+    free(g);
+    g = NULL;
+    return 1;
+    // N tem como retornar 0 caso ocorra erro
+    // Depois de um free ocorre undefined behaviour
+    //
+    // TODO: Passar por todos os vertices desalocando
 }
 
 lista vizinhanca(vertice v, int direcao, grafo g){
-  no atual = g->vertices->primeiro;
+    no atual = g->vertices->primeiro;
 
-  while((vertice)conteudo(atual) != v){
-    atual = vertice->proximo;
-    // TODO: verificar caso o vertice nao esteja no grafo
-  }
+    while((vertice)conteudo(atual) != v){
+        atual = vertice->proximo;
+        // TODO: verificar caso o vertice nao esteja no grafo
+    }
 
-  switch (direcao) {
-    case 0:
-      return (vertice)conteudo(atual)->grau;
-    break;
-    case -1:
-      return (vertice)conteudo(atual)->grau_ent;
-    break;
-    case 1:
-      return (vertice)conteudo(atual)->grau_sai;
-    break;
-    default:
-    // TODO: retorno de erro
-  }
+    switch (direcao) {
+        case 0:
+        return (vertice)conteudo(atual)->grau;
+        break;
+        case -1:
+        return (vertice)conteudo(atual)->grau_ent;
+        break;
+        case 1:
+        return (vertice)conteudo(atual)->grau_sai;
+        break;
+        default:
+        // TODO: retorno de erro
+    }
 }
